4e0a32fc1cabf45c06c1e34d43690fd095b76857
Oleeveira <octo20048@gmail.com>
2024-10-30 10:20:48 -0300

    sign up screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
 import 'package:greengrocer/src/config/custon_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -162,7 +163,15 @@ class SignInScreen extends StatelessWidget {
                             color: Colors.green
                           )
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (c){
+                                return SignUpScreen();
+                              }
+                            ),
+                          );
+                        },
                         child: const Text('Criar conta', 
                           style: TextStyle(
                             fontSize: 18,
